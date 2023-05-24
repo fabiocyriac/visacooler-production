8cf1d51a9502fb2b5b3dc37fbd1ce69fa03ce4cc
Fabio Cyriac <fabiocyriac@fabios-air.home>
2023-05-24 13:06:31 +0100

    hover issue

--- client/src/assets/wrappers/StatItem.js
+++ client/src/assets/wrappers/StatItem.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 const Wrapper = styled.article`
   padding: 2rem;
-  cursor: pointer;
   background: var(--white);
+  border-radius: var(--borderRadius);
   border-bottom: 5px solid ${(props) => props.color};
   header {
     display: flex;
