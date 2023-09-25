1fc7ec9efa7860dfa1fe28957f4916994ef2f955
Fabio Cyriac <fabiocyriac123@gmail.com>
2023-09-25 12:16:18 +0100

    updating assets

---
+++ client/src/assets/wrappers/VisaInfo.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin-top: 0.5rem;
+  display: flex;
+  align-items: center;
+
+  .icon {
+    font-size: 1rem;
+    margin-right: 1rem;
+    display: flex;
+    align-items: center;
+    svg {
+      color: var(--grey-400);
+    }
+  }
+  .text1 {
+    color: var(--grey-400);
+    padding-right: 3px;
+    text-transform: capitalize;
+    letter-spacing: var(--letterSpacing);
+  }
+  .text2 {
+    text-transform: capitalize;
+    letter-spacing: var(--letterSpacing);
+    
+  }
+`
+export default Wrapper
